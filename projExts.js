163a43fc445f9f17496ee13f2b620debbcdc9ec0
nash <nash@iffy.me>
2019-09-23 16:23:28 +0800

    .

--- projExts.js
+++ projExts.js
@@ -1,18 +1,21 @@
 const matchRegexp = require('./matchRegexp')
 const matcher = matchRegexp(require('./manifest'))
-const registry = process.env.PKG_REGISTRY
+
+const registry = 'http://172.178.1.204:2015/vue-components'
 const externalModules = registry ? JSON.parse(require("child_process").execSync(`curl -# ${registry}/${process.env.PKG_INDEX || 'latest.json'}`)) : {}
+console.log(externalModules)
+
 
 module.exports = {
     projExts({ name, callback }) {
         if (name === 'vue') {
             callback()
         } else if (name in externalModules) {
-            callback(null, `() => externalComponent('${registry}','${request}.${externalModules[request]}')`)
+            callback(null, `() => externalComponent('${registry}','${name}.${externalModules[name]}')`)
         } else {
             let comp = matcher(name)
             if (comp && comp in externalModules) {
-                callback(null, `() => externalComponent('${registry}','${externalModules[comp]}')`)
+                callback(null, `() => externalComponent('${registry}','${comp}.${externalModules[comp]}')`)
             } else {
                 callback()
             }
